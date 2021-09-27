213998d02970b1ea8f6554828326a1ae794dd4f2
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-27 17:13:33 +0530

    Calculate the length of two points

---
+++ UC1LineComparision/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UC1LineComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter X1 value");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter X2 value");
+            int x2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Y1 value");
+           int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Y2 value");
+            int y2 = int.Parse(Console.ReadLine());
+
+            double length= Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1)* (y2 - y1));
+            Console.WriteLine("Length is : "+length);
+
+        }
+    }
+}
