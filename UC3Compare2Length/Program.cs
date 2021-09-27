9c755073d89d1ec25a55d968fd7d18f8e4a30583
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-27 17:19:23 +0530

    Compare one line is equal or greater or less than other line

---
+++ UC3Compare2Length/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UC3Compare2Length
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter coordinates for First line");
+            Console.WriteLine("Enter values of the coordinates for first point(x1,y1)");
+            x1 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter values of the coordinates for second point(x2,y2)");
+            x2 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+
+            int a1, a2, b1, b2;
+            Console.WriteLine("Enter coordintes for Second Line");
+            Console.WriteLine("Enter values of the coordinates for first point(a1,b1)");
+            a1 = int.Parse(Console.ReadLine());
+            b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter values of the coordinates for second point(a2,b2)");
+            a2 = int.Parse(Console.ReadLine());
+            b2 = int.Parse(Console.ReadLine());
+
+            Neww obj = new Neww();
+            double length1 = obj.getLength(x1, y1, x2, y2);
+            double length2 = obj.getLength(a1, b1, a2, b2);
+
+            int cmprVal = length1.CompareTo(length2);
+            if(cmprVal==0)
+                Console.WriteLine("Value of both the lines are equal");
+            else if(cmprVal>0)
+                Console.WriteLine("Value of line1 is greater than line2");
+            else
+                Console.WriteLine("Value of line1 is smaller than line2");
+        }
+    }
+}
