5211c29a148808bae7192f64cdc4661492ef0989
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-27 17:21:11 +0530

    Compare Lines using Object oriented Programming concept

---
+++ UC4LineCmpr/Neww.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC4LineCmpr
+{
+    class Neww
+    {
+        public double getLength(int x1, int y1, int x2, int y2)
+        {
+            double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("Length is : " + length);
+            return length;
+        }
+
+        public void cmprValue(double len1,double len2)
+        {
+            int cmprVal = len1.CompareTo(len2);
+            if (cmprVal == 0)
+                Console.WriteLine("Value of both the lines are equal");
+            else if (cmprVal > 0)
+                Console.WriteLine("Value of line1 is greater than line2");
+            else
+                Console.WriteLine("Value of line1 is smaller than line2");
+        }
+        public void isEqual(double len1, double len2)
+        {
+            if (len1.Equals(len2))
+                Console.WriteLine("Value of both the lines are equal");
+            else
+                Console.WriteLine("Value of both the lines are not equal");
+        }
+    }
+
+    
+}
+
---
+++ UC4LineCmpr/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace UC4LineCmpr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter coordinates for First line");
+            Console.WriteLine("Enter values of the coordinates for first point(x1,y1)");
+            x1 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter values of the coordinates for second point(x2,y2)");
+            x2 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+
+            int a1, a2, b1, b2;
+            Console.WriteLine("Enter coordintes for Second Line");
+            Console.WriteLine("Enter values of the coordinates for first point(a1,b1)");
+            a1 = int.Parse(Console.ReadLine());
+            b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter values of the coordinates for second point(a2,b2)");
+            a2 = int.Parse(Console.ReadLine());
+            b2 = int.Parse(Console.ReadLine());
+
+            Neww obj = new Neww();
+            double length1 = obj.getLength(x1, y1, x2, y2);
+            double length2 = obj.getLength(a1, b1, a2, b2);
+
+            obj.cmprValue(length1, length2);
+            obj.isEqual(length1, length2);
+
+            
+
+
+
+        }
+    }
+}
