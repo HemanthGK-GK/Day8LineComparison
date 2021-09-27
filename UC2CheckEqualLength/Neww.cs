bf4ae9f5922fc56110c4ddc57f035f9d6a59670d
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-27 17:16:49 +0530

    Compare two lines are equal or not

---
+++ UC2CheckEqualLength/Neww.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2CheckEqualLength
+{
+    class Neww
+    {
+        public double getLength(int x1, int y1, int x2, int y2)
+        {
+            double length = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            Console.WriteLine("Length is : " + length);
+            return length;
+        }
+    }
+}
---
+++ UC2CheckEqualLength/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace UC2CheckEqualLength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x1, x2, y1, y2;
+            Console.WriteLine("Enter coordinates for First line");
+            Console.WriteLine("Enter values of the coordinates for first point(x1,y1)");
+            x1 = int.Parse(Console.ReadLine());
+            y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter values of the coordinates for second point(x2,y2)");
+            x2 = int.Parse(Console.ReadLine());
+            y2 = int.Parse(Console.ReadLine());
+
+            int a1, a2, b1, b2;
+            Console.WriteLine("Enter coordintes for Second Line");
+            Console.WriteLine("Enter values of the coordinates for first point(a1,b1)");
+            a1 = int.Parse(Console.ReadLine());
+            b1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter values of the coordinates for second point(a2,b2)");
+            a2 = int.Parse(Console.ReadLine());
+            b2 = int.Parse(Console.ReadLine());
+
+            Neww obj = new Neww();
+            double length1 = obj.getLength(x1, y1, x2, y2);
+            double length2 = obj.getLength(a1, b1, a2, b2);
+
+            if (length1.Equals(length2))
+                Console.WriteLine("Value of both the lines are equal");
+            else
+                Console.WriteLine("Value of both the lines are not equal");
+
+
+        }
+    }
+}
